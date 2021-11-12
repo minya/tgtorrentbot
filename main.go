06b36de7ce52d204bdfec251cd0ba783d6608ba7
Mikhail Brel <minya@skbkontur.ru>
2021-11-12 15:57:18 +0500

    get settings from env

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"time"
 
 	"github.com/minya/telegram"
@@ -11,12 +13,10 @@ import (
 )
 
 func main() {
-	settingsBytes, err := ioutil.ReadFile("settings.json")
+	settings, err := readSettings()
 	if err != nil {
 		log.Fatal(err)
 	}
-	var settings Settings
-	json.Unmarshal(settingsBytes, &settings)
 	api := telegram.NewApi(settings.BotToken)
 
 	conf := transmission.Config{
@@ -35,4 +35,40 @@ func main() {
 		downloadPath:       settings.DownloadPath,
 	}
 	log.Fatal(telegram.StartPolling(&api, handler.HandleUpdate, 3*time.Second, -1))
+}
+
+func readSettings() (Settings, error) {
+	settings, err := readSettingsFromEnv()
+	if err == nil {
+		return settings, nil
+	}
+	settings, err = readSettingsFromFile()
+	if err == nil {
+		return settings, nil
+	}
+	return Settings{}, fmt.Errorf("cant get settings")
+}
+
+func readSettingsFromEnv() (Settings, error) {
+	var settings Settings
+	settings.BotToken = os.Getenv("TGT_BOTTOKEN")
+	settings.DownloadPath = os.Getenv("TGT_DOWNLOADPATH")
+	settings.TransmissionRPC.Address = os.Getenv("TGT_RPC_ADDR")
+	settings.TransmissionRPC.User = os.Getenv("TGT_RPC_USER")
+	settings.TransmissionRPC.Password = os.Getenv("TGT_RPC_PASSWORD")
+
+	if settings.BotToken == "" || settings.DownloadPath == "" || settings.TransmissionRPC.Address == "" {
+		return settings, fmt.Errorf("can't get settings from env")
+	}
+	return settings, nil
+}
+
+func readSettingsFromFile() (Settings, error) {
+	settingsBytes, err := ioutil.ReadFile("settings.json")
+	var settings Settings
+	if err != nil {
+		return settings, err
+	}
+	json.Unmarshal(settingsBytes, &settings)
+	return settings, nil
 }
